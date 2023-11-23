f65cd8e2a276ba49859d3c52398110dd94fc6349
Alisher <alisherxujanov163@gmail.com>
2023-11-23 19:53:24 +0500

    HOMEWORK

--- main.py
+++ main.py
@@ -1,24 +1,29 @@
+# HOMEWORK
+# 1. Create a new repository on Github
+# 2. Create a new project called my_project
+# 3. Get 1-beginner file and do all tasks
+
+
+
+
 # JavaScript Object Notation (JSON)
 import requests
 from faker import Faker
 import json
-
-
-# In js we would do:
-# axios.get('https://...')
+URL = 'https://jsonplaceholder.typicode.com/todos/1'
 
 # In python we do:
-response = requests.get('https://...')
+response = requests.get(URL)
 
 # To get the status code:
-print(response.status_code)
+print("status_code: ", response.status_code)
 
 # To get the content:
-print(response.content)
+print("content: ", response.content)
 
 # To get the json:
-print(response.json())
-
+print("json(): ", response.json())
+# ===================================================================
 
 # JSON.stringify(data)
 # JSON.parse(data)
